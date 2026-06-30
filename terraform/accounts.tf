8afaaacc42fe928037403a9554a9023f40030bd8
Jordan Nelson <jordandn6@outlook.com>
2026-06-30 18:03:01 -0500

    Fix CI: validate without AWS credentials

--- terraform/accounts.tf
+++ terraform/accounts.tf
@@ -1,6 +1,6 @@
 resource "aws_organizations_account" "sandbox" {
   name      = "sandbox"
-  email     = "${replace(var.org_email_domain, "@", "+sandbox@")}"
+  email     = replace(var.org_email_domain, "@", "+sandbox@")
   parent_id = aws_organizations_organizational_unit.sandbox.id
 
   role_name = "OrganizationAccountAccessRole"
@@ -14,7 +14,7 @@ resource "aws_organizations_account" "sandbox" {
 
 resource "aws_organizations_account" "dev" {
   name      = "dev"
-  email     = "${replace(var.org_email_domain, "@", "+dev@")}"
+  email     = replace(var.org_email_domain, "@", "+dev@")
   parent_id = aws_organizations_organizational_unit.dev.id
 
   role_name = "OrganizationAccountAccessRole"
@@ -28,7 +28,7 @@ resource "aws_organizations_account" "dev" {
 
 resource "aws_organizations_account" "prod" {
   name      = "prod"
-  email     = "${replace(var.org_email_domain, "@", "+prod@")}"
+  email     = replace(var.org_email_domain, "@", "+prod@")
   parent_id = aws_organizations_organizational_unit.prod.id
 
   role_name = "OrganizationAccountAccessRole"
